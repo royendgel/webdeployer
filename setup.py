51d766c714ad5792f033d3f2c548cf8a13c1f2bd
royendgelsilberie <royendgel@gmail.com>
2014-04-24 22:15:04 -0400

    working module from private

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 readme = open('README.md').read()
@@ -21,8 +17,8 @@ setup(
     classifiers=[
       'Operating System :: MacOS :: MacOS X',
       'Operating System :: Unix',
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        'Programming Language :: Python :: 2.7',
+      'Development Status :: 3 - Alpha',
+      'Topic :: Utilities',
+      'Programming Language :: Python :: 2.7',
     ],
 )
